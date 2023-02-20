2e1b2b5b14f76b1bc4acd56d6d332a2a52382aa0
therealsweven <robssimpson11@gmail.com>
2023-02-20 13:04:57 -0700

    Added hour elements, and pulls id from parent element of target of event listener to select the proper hour

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +5,12 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  var saveBtn = $(".saveBtn");
+  saveBtn.on("click", function (event) {
+    saveHourElChild = $(this);
+    savHour = saveHourElChild.parent().attr("id");
+  });
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
